4608c71ce6bbaf5b5b9cbd40f82b6cb266d86cd8
YousefXI <101951776+YousefXI@users.noreply.github.com>
2022-08-27 22:22:09 +0200

    first output password check

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const indicator = document.querySelector(".indicator");
 const input = document.querySelector("input");
 const weak = document.querySelector(".weak");
 const medium = document.querySelector(".medium");
-const strong = document.querySelector(".storng");
+const strong = document.querySelector(".strong");
+const text = document.querySelector(".text");
 let regExpWeak = /[a-z]/;
 let regExpMedium = /\d+/;
-
 let regExpStrong = /.[!,@,#,$,%,^,&,*,?,_,~,-,(,)]/;
+
 function trigger() {
    if (input.value != "") {
       indicator.style.display = "block";
@@ -20,13 +21,30 @@ function trigger() {
          no = 1;
 
       if (
-         input.value.length <= 6 &&
+         input.value.length >= 6 &&
          ((input.value.match(regExpWeak) && input.value.match(regExpMedium)) ||
             (input.value.match(regExpMedium) &&
                input.value.match(regExpStrong)) ||
             (input.value.match(regExpWeak) && input.value.match(regExpStrong)))
       )
          no = 1;
+
+      if (
+         input.value.length >= 6 &&
+         ((input.value.match(regExpWeak) && input.value.match(regExpMedium)) ||
+            (input.value.match(regExpMedium) &&
+               input.value.match(regExpStrong) &&
+               input.value.match(regExpWeak) &&
+               input.value.match(regExpStrong)))
+      )
+         no = 3;
+
+      if (no == 1) {
+         weak.classList.add("active");
+         text.style.display = "block";
+         text.textContent = "Your password is too weak";
+         text.classList.add("weak");
+      }
    } else {
       indicator.style.display = "none";
    }
